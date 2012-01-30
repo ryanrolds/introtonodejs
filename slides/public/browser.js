c3275fee4f4dd3041f4bda8f9bf76b0c20a9806b
Ryan Olds <ryanrolds@gmail.com>
2012-01-29 19:52:44 -0600

    Moved some stuff around. Put socket.io example in slides

---
+++ slides/public/browser.js
@@ -0,0 +1,39 @@
+var socket = io.connect(window.location.href);
+
+// Listen for mouse moves and send them to the server
+$(document).one('ready', function() {
+  $(document).mousemove(function(event) {
+    socket.emit('move', {'x': event.pageX, 'y': event.pageY});
+  });
+});
+
+// Handle peer movements
+socket.on('peer move', function(data) {
+  // Get the cursor
+  var cursor = ($('#cursor_' + data.id).length) ? $('#cursor_' + data.id)[0] : null;
+
+  if(cursor === null) {
+    // Create element
+    cursor = document.createElement('div');
+    cursor.id = 'cursor_' + data.id;
+    cursor.className = 'cursor';
+
+    // Add to document
+    document.getElementsByTagName('body')[0].appendChild(cursor);
+    updateTitle();
+  }
+
+  // Set position
+  cursor.style.left = data.x + 'px';
+  cursor.style.top = data.y + 'px';
+});
+
+// Handle disconnects
+socket.on('peer disconnect', function(data) {
+  $('#cursor_' + data.id).remove();
+  updateTitle();
+});
+
+function updateTitle() {
+    document.title = 'Introduction to Node.js :: ' + $('.cursor').length + ' viewer(s)';
+}
--- slides/server.js
+++ slides/server.js
@@ -1,17 +1,45 @@
 
 var fs = require('fs');
 var express = require('express');
-
-var port = 8081;
 var app = express.createServer();
 
-app.configure(function() {
-  app.use(express.static(__dirname)),
-  app.use(function(req, res) {
-    res.redirect('/index.html');
-  })
+var port = 9001;
+
+// Server browser js and cursor image
+app.use(express.static(__dirname + '/public'));
+app.use(function(req, res) {
+  res.redirect('/index.html');
 });
 
+// Collection of connected clients
+var clients = {};
+
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function(socket) { 
+  var id = socket.id;
+  clients[id] = socket; // Add connection to list
+
+  socket.on('move', function(data) {
+    sendToPeers(id, 'peer move', {'id': id, 'x': data.x, 'y': data.y});
+  });
+
+  socket.on('disconnect', function(socket) {
+    delete clients[id]; // Remove conection from list
+    sendToPeers(id, 'peer disconnect', {'id': id});
+  });
+});
 
 app.listen(port);
 console.log('Listening on %d', port);
+
+// Helper function to handle relaying data to peers
+function sendToPeers(ownId, event, message) {
+  for(id in clients) {
+    if(id === ownId) {
+      continue;
+    }
+
+    clients[id].emit(event, message);
+  }
+}
+
